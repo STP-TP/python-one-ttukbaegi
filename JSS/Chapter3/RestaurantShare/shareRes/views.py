6634a91d5b089bbdc5ce7726cb84f1280fd7facd
jooss287 <jooss287@gmail.com>
2022-05-02 01:25:49 +0900

    [#3.1.3] 맛집 추가 및 상세보기 페이지 제작
    ## 작업내용
    * 맛집 추가 기능 제작
    * 맛집 상세보기 페이지 기본기능 완료

--- JSS/Chapter3/RestaurantShare/shareRes/views.py
+++ JSS/Chapter3/RestaurantShare/shareRes/views.py
@@ -8,19 +8,37 @@ from .models import *
 
 def index(request):
     categories = Category.objects.all()
-    content = {'categories': categories}
+    restaurants = Restaurant.objects.all()
+    content = {'categories': categories, 'restaurants': restaurants}
     # return HttpResponse("index")
     return render(request, 'shareRes/index.html', content)
 
 
-def restaurantDetail(request):
+def restaurantDetail(request, res_id):
+    restaurant = Restaurant.objects.get(id=res_id)
+    content = {'restaurant': restaurant}
     # return HttpResponse("resstaurantDetail")
-    return render(request, 'shareRes/restaurantDetail.html')
+    return render(request, 'shareRes/restaurantDetail.html', content)
 
 
 def restaurantCreate(request):
+    categories = Category.objects.all()
+    content = {'categories': categories}
     # return HttpResponse("restaurantCreate")
-    return render(request, 'shareRes/restaurantCreate.html')
+    return render(request, 'shareRes/restaurantCreate.html', content)
+
+
+def Create_restaurant(request):
+    category_id = request.POST['resCategory']
+    category = Category.objects.get(id=category_id)
+    name = request.POST['resTitle']
+    link = request.POST['resLink']
+    content = request.POST['resContent']
+    keyword = request.POST['resLoc']
+    new_res = Restaurant(category=category, restaurant_name=name,
+                         restaurant_link=link, restaurant_content=content, restaurant_keyword=keyword)
+    new_res.save()
+    return HttpResponseRedirect(reverse('index'))
 
 
 def categoryCreate(request):
