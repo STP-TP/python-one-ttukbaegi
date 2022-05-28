d40c132e32997e34f181bfad18f443420244bb49
jooss287 <jooss287@gmail.com>
2022-05-28 22:21:27 +0900

    [#4.1.0] 각 Chapter Django Secret key 교체
    ## 작업내용
    * Github에 Secret key push 됨
    * secret key 새로 등록하고 공개되지 않도록 수정

--- JSS/Chapter3/RestaurantShare/RestaurantShare/settings.py
+++ JSS/Chapter3/RestaurantShare/RestaurantShare/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from .env.secret_info import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_wqyk&$diw%wg=nc3&21^a&az^(rd6jh1=p_*6asa3q57bz4+a'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- JSS/Chapter4/ExcelCalculate/ExcelCalculate/settings.py
+++ JSS/Chapter4/ExcelCalculate/ExcelCalculate/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from .env.mail_info import *
+from .env.secret_info import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8m9*txd91*t1jtvg#^$3@w#x+#w*pc9e%ntcpc1jyw*(=o8z2y'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
